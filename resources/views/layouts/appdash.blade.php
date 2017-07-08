f5d3cc5eeb5be92bf476db214a1393858f2be070
Joel Roberts <newfoundfreedom@gmail.com>
2017-07-08 15:44:26 -0400

    discover page

--- resources/views/layouts/appdash.blade.php
+++ resources/views/layouts/appdash.blade.php
@@ -15,54 +15,56 @@
     <link href="https://file.myfontastic.com/cmbgRkayBnHXSnPo8TJuHj/icons.css" rel="stylesheet">
 </head>
 <body>
-    <div id="app">
+<div id="app">
 
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-sm-1">
-                    <nav class="navbar navbar-default navbar-fixed-side">
-                        <!-- normal collapsible navbar markup -->
-                        <!-- Brand and toggle get grouped for better mobile display -->
-                        <div class="navbar-header">
-                            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#sidebar-collapse" aria-expanded="false">
-                                <span class="sr-only">Toggle navigation</span>
-                                <span class="icon-bar"></span>
-                                <span class="icon-bar"></span>
-                                <span class="icon-bar"></span>
-                            </button>
-                            <a class="navbar-brand" href="#">Brand</a>
-                        </div>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-sm-1">
+                <nav class="navbar navbar-default navbar-fixed-side">
+                    <!-- normal collapsible navbar markup -->
+                    <!-- Brand and toggle get grouped for better mobile display -->
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
+                                data-target="#sidebar-collapse" aria-expanded="false">
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                        <a class="navbar-brand" href="#">Brand</a>
+                    </div>
 
-                        <!-- Collect the nav links, forms, and other content for toggling -->
-                        <div class="collapse navbar-collapse" id="sidebar-collapse">
-                            <ul class="nav navbar-nav">
-                                <li class="text-center"><a href="#"><i class="trvlrs-calendar"></i></a></li>
-                                <li class="text-center"><a href="/invite"><i class="trvlrs-mail"></i></a></li>
-                                <li class="text-center"><a href="/discovery"><i class="trvlrs-location"></i></a></li>
-                                <li class="text-center"><a href="#"><i class="trvlrs-banknote"></i></a></li>
-                            </ul>
-                        </div><!-- /.navbar-collapse -->
-                    </nav>
-                </div>
-                <div class="col-sm-11">
-                    <!-- your page content -->
-                    <div class="row">
-                        <div class="col-sm-7" id="left-column" style="background-color: lightslategrey">
-                         <!-- Ya'll put shit in here for the left -->
-                            @yield('leftcontent')
-                        </div>
+                    <!-- Collect the nav links, forms, and other content for toggling -->
+                    <div class="collapse navbar-collapse" id="sidebar-collapse">
+                        <ul class="nav navbar-nav">
+                            <li class="text-center"><a href="#"><i class="trvlrs-calendar"></i></a></li>
+                            <li class="text-center"><a href="/invite"><i class="trvlrs-mail"></i></a></li>
+                            <li class="text-center"><a href="/discovery"><i class="trvlrs-location"></i></a></li>
+                            <li class="text-center"><a href="#"><i class="trvlrs-banknote"></i></a></li>
+                        </ul>
+                    </div><!-- /.navbar-collapse -->
+                </nav>
+            </div>
+            <div class="col-sm-11">
+                <!-- your page content -->
+                @yield('fullcontent')
+                <div class="row">
+                    <div class="col-sm-7" id="left-column" style="background-color: lightslategrey">
+                        <!-- Ya'll put shit in here for the left -->
+                        @yield('leftcontent')
+                    </div>
 
-                        <div class="col-sm-5" id="right-column" style="background-color: lightblue">
-                            <!-- Ya'll put shit in here for the right -->
-                            @yield('rightcontent')
-                        </div>
+                    <div class="col-sm-5" id="right-column" style="background-color: lightblue">
+                        <!-- Ya'll put shit in here for the right -->
+                        @yield('rightcontent')
                     </div>
                 </div>
             </div>
         </div>
     </div>
+</div>
 
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}"></script>
+<!-- Scripts -->
+<script src="{{ asset('js/app.js') }}"></script>
 </body>
 </html>
--- resources/views/pages/discovery.blade.php
+++ resources/views/pages/discovery.blade.php
@@ -122,102 +122,105 @@
         $body = json_decode($response);
         $bearer_token = $body->access_token;
         ?>
-
-        <table width="100%" border="0" align="left" cellpadding="5" cellspacing="0" class="box" style="color: #000000">
-            <tr>
-                <th width="5%" align="left" class="auto-style7">#</th>
-                <th width="10%" align="left" class="auto-style7">Image</th>
-                <th width="15%" align="left" class="auto-style7">Business Name</th>
-                <th width="35%" align="left" class="auto-style7">Address</th>
-                <th width="15%" align="left" class="auto-style7">Phone</th>
-                <th width="5%" align="left" class="auto-style7">Categories</th>
-                <th width="5%" align="left" class="auto-style7">Reviews</th>
-                <th width="5%" align="left" class="auto-style7">Rating</th>
-                <th width="5%" align="left" class="auto-style7">Closed</th>
-            </tr>
-            <?php
-            $class = "row2";
-            $sarray = array();
-            $sarray[] = "Business Name|DELIMITER|Address|DELIMITER|Phone|DELIMITER|Reviews|DELIMITER|Rating|DELIMITER|Categories|DELIMITER|Closed|DELIMITER|URL";
-            if($keywords != ""){
-            $count = 1;
-            for($i = 0;$i <= $max;$i = $i + 50){
-
-
-            if ($mile == "0") {
-                $yelp_url = "https://api.yelp.com/v3/businesses/search?term=" . urlencode($keywords) . "&location=" . urlencode($location) . "&limit=50&offset=" . $i;
-            } else {
-                $yelp_url = "https://api.yelp.com/v3/businesses/search?term=" . urlencode($keywords) . "&location=" . urlencode($location) . "&limit=50&radius=" . $mile . "&offset=" . $i;
-            }
-
-            $curl = curl_init();
-
-            curl_setopt_array($curl, array(
-                CURLOPT_URL => $yelp_url,
-                CURLOPT_RETURNTRANSFER => true,  // Capture response.
-                CURLOPT_SSL_VERIFYPEER => false,
-                CURLOPT_HTTPHEADER => array(
-                    "authorization: Bearer " . $bearer_token,
-                    "cache-control: no-cache",
-                ),
-            ));
-
-            $response = curl_exec($curl);
-            curl_close($curl);
-            $response = json_decode($response);
-
-            foreach($response->businesses as $business){
-            if ($business->image_url != "") $image = "<a href='" . $business->image_url . "' target='_blank'><img border=0 height='100' width='100' src='" . str_replace("o.jpg", "ms.jpg", $business->image_url) . "' /></a>";
-            $name = $business->name;
-            $cla = $business->is_closed;
-            $url = $business->url;
-            $title = "<a href='" . $url . "' target='_blank'>" . $name . "</a>";
-            $phone = $business->display_phone;
-            $reviews = $business->review_count;
-            $rating = $business->rating;
-            $address = $business->location->display_address[0] . " " . $business->location->display_address[1] . " " . $business->location->display_address[2] . " " . $business->location->display_address[3] . " " . $business->location->display_address[4];
-            $address = trim($address);
-            $address = preg_replace('/\s+/', ' ', $address);
-            $categories = "";
-            foreach ($business->categories as $cat) {
-                $categories .= $cat->title . ", ";
-            }
-            $categories = rtrim($categories, ", ");
-            if ($cla) {
-                $closed = "Yes";
-            } else {
-                $closed = "No";
-            }
+            <table width="100%" border="0" align="left" cellpadding="5" cellspacing="0" class="box"
+                   style="color: #000000">
+                <tr>
+                    <th width="5%" align="left" class="auto-style7">#</th>
+                    <th width="10%" align="left" class="auto-style7">Image</th>
+                    <th width="15%" align="left" class="auto-style7">Business Name</th>
+                    <th width="35%" align="left" class="auto-style7">Address</th>
+                    <th width="15%" align="left" class="auto-style7">Phone</th>
+                    <th width="5%" align="left" class="auto-style7">Categories</th>
+                    <th width="5%" align="left" class="auto-style7">Reviews</th>
+                    <th width="5%" align="left" class="auto-style7">Rating</th>
+                    <th width="5%" align="left" class="auto-style7">Closed</th>
+                </tr>
+        <?php
+        $class = "row2";
+        $sarray = array();
+        $sarray[] = "Business Name|DELIMITER|Address|DELIMITER|Phone|DELIMITER|Reviews|DELIMITER|Rating|DELIMITER|Categories|DELIMITER|Closed|DELIMITER|URL";
+        if($keywords != ""){
+        $count = 1;
+        for($i = 0;$i <= $max;$i = $i + 50){
 
 
-            $class = ($class == "row2") ? "row1" : "row2";
-            ?>
+        if ($mile == "0") {
+            $yelp_url = "https://api.yelp.com/v3/businesses/search?term=" . urlencode($keywords) . "&location=" . urlencode($location) . "&limit=50&offset=" . $i;
+        } else {
+            $yelp_url = "https://api.yelp.com/v3/businesses/search?term=" . urlencode($keywords) . "&location=" . urlencode($location) . "&limit=50&radius=" . $mile . "&offset=" . $i;
+        }
 
-            <tr class="<?php echo $class ?>">
-                <td class="parent auto-style7"><?php echo $count ?></td>
-                <td class="parent auto-style7"><?php echo $image ?></td>
-                <td class="parent auto-style7"><?php echo $title ?></td>
-                <td class="parent auto-style7"><?php echo $address ?></td>
-                <td class="parent auto-style7"><?php echo $phone ?></td>
-                <td class="parent auto-style7"><?php echo $categories ?></td>
-                <td class="parent auto-style7"><?php echo $reviews ?></td>
-                <td class="parent auto-style7"><?php echo $rating ?></td>
-                <td class="parent auto-style7"><?php echo $closed ?></td>
-            </tr>
+        $curl = curl_init();
 
-            <?php
-            $sarray[] = $name . "|DELIMITER|" . $address . "|DELIMITER|" . str_replace("-", " ", $phone) . "|DELIMITER|" . $reviews . "|DELIMITER|" . $rating . "|DELIMITER|" . $categories . "|DELIMITER|" . $closed . "|DELIMITER|" . $url;
-            $count++;
-            }
-            }
-            }
-            ?>
-            <tr align="center">
-                <td colspan="9"><p align="center" class="auto-style5">
-                    <?php    echo '<center><form action="/discovery" method="POST"><input name="download" type="hidden" value="' . base64_encode(serialize($sarray)) . '" size="30"><input class="button" type="submit" value="Export Data"></form></center>'; ?>
-                </td>
-            </tr>
-        </table>
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => $yelp_url,
+            CURLOPT_RETURNTRANSFER => true,  // Capture response.
+            CURLOPT_SSL_VERIFYPEER => false,
+            CURLOPT_HTTPHEADER => array(
+                "authorization: Bearer " . $bearer_token,
+                "cache-control: no-cache",
+            ),
+        ));
+
+        $response = curl_exec($curl);
+        curl_close($curl);
+        $response = json_decode($response);
+
+
+        foreach($response->businesses as $business){
+        if ($business->image_url != "") $image = "<a href='" . $business->image_url . "' target='_blank'><img border=0 height='100' width='100' src='" . str_replace("o.jpg", "ms.jpg", $business->image_url) . "' /></a>";
+        $name = $business->name;
+        $cla = $business->is_closed;
+        $url = $business->url;
+        $title = "<a href='" . $url . "' target='_blank'>" . $name . "</a>";
+        $phone = $business->display_phone;
+        $reviews = $business->review_count;
+        $rating = $business->rating;
+        $address = $business->location->display_address[0] . " " . $business->location->display_address[1] . " " . $business->location->display_address[2] . " " . $business->location->display_address[3] . " " . $business->location->display_address[4];
+        $address = trim($address);
+        $address = preg_replace('/\s+/', ' ', $address);
+        $categories = "";
+        foreach ($business->categories as $cat) {
+            $categories .= $cat->title . ", ";
+        }
+        $categories = rtrim($categories, ", ");
+        if ($cla) {
+            $closed = "Yes";
+        } else {
+            $closed = "No";
+        }
+
+
+
+        $class = ($class == "row2") ? "row1" : "row2";
+        ?>
+
+
+        <tr class="<?php echo $class ?>">
+            <td class="parent auto-style7"><?php echo $count ?></td>
+            <td class="parent auto-style7"><?php echo $image ?></td>
+            <td class="parent auto-style7"><?php echo $title ?></td>
+            <td class="parent auto-style7"><?php echo $address ?></td>
+            <td class="parent auto-style7"><?php echo $phone ?></td>
+            <td class="parent auto-style7"><?php echo $categories ?></td>
+            <td class="parent auto-style7"><?php echo $reviews ?></td>
+            <td class="parent auto-style7"><?php echo $rating ?></td>
+            <td class="parent auto-style7"><?php echo $closed ?></td>
+        </tr>
+
+        <?php
+        $sarray[] = $name . "|DELIMITER|" . $address . "|DELIMITER|" . str_replace("-", " ", $phone) . "|DELIMITER|" . $reviews . "|DELIMITER|" . $rating . "|DELIMITER|" . $categories . "|DELIMITER|" . $closed . "|DELIMITER|" . $url;
+        $count++;
+        }
+        }
+        }
+        ?>
+        <tr align="center">
+            <td colspan="9"><p align="center" class="auto-style5">
+                <?php    echo '<center><form action="/discovery" method="POST"><input name="download" type="hidden" value="' . base64_encode(serialize($sarray)) . '" size="30"><input class="button" type="submit" value="Export Data"></form></center>'; ?>
+            </td>
+        </tr>
+        {{--</table>--}}
         <?php
         }
         }
