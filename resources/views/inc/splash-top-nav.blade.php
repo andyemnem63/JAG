35ab030f81409bd6187a0d4ecb25521e3f0fc8ea
Joel Roberts <newfoundfreedom@gmail.com>
2017-07-09 23:03:36 -0400

    updated images folder

--- resources/views/inc/splash-top-nav.blade.php
+++ resources/views/inc/splash-top-nav.blade.php
@@ -13,7 +13,7 @@
 
             <!-- Branding Image -->
             <a class="navbar-brand" href="{{ url('/') }}">
-                <img src="{{ asset('img/trvlrs-logo.svg') }}" alt="trvlrs logo" height="33px">
+                <img src="{{ asset('images') }}" alt="trvlrs logo" height="33px">
             </a>
         </div>
 
