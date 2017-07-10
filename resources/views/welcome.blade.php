24a364de27fab73e2c0892baa601cc48b84bd1ed
Joel Roberts <newfoundfreedom@gmail.com>
2017-07-09 23:43:19 -0400

    fixed image problems and background color for site

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,16 +5,24 @@
 
     <div class="container">
         <div class="row text-center">
-            <img src="{{ asset('images') }}" alt="trvlrs logo" class="welcome-logo">
+            <img src="{{ asset('images/trvlrs-logo.svg') }}" alt="trvlrs logo" class="welcome-logo">
         </div>
         <div class="text-center blanco">
             <h1>Plan an awesome trip together.</h1>
             <p>Get started with trvlrs free group trip planner.</p>
             <br>
-            <a href="{{ route('login') }}">
-                <button class="btn btn-lg btn-primary text-center">Start Planning Your Trip &nbsp;&nbsp;<i
-                            class="glyphicon glyphicon-chevron-right"></i></button>
-            </a>
+            @if (Auth::guest())
+                <a href="{{ route('login') }}">
+                    <button class="btn btn-lg btn-primary text-center">Start Planning Your Trip &nbsp;&nbsp;<i
+                                class="glyphicon glyphicon-chevron-right"></i></button>
+                </a>
+            @else
+                <a href="/account">
+                    <button class="btn btn-lg btn-primary text-center">Start Planning Your Trip &nbsp;&nbsp;<i
+                                class="glyphicon glyphicon-chevron-right"></i></button>
+                </a>
+            @endif
+
         </div>
     </div>
 
