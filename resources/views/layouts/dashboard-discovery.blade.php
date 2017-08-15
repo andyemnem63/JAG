bbd08b0eb07fb0a4913783c5cc764b9b055dde63
Glen Turner <gturner0316@yahoo.com>
2017-08-15 17:51:07 -0400

    style

--- resources/views/layouts/dashboard-discovery.blade.php
+++ resources/views/layouts/dashboard-discovery.blade.php
@@ -13,7 +13,7 @@
     <!-- Styles -->
     <link href="{{ secure_asset('css/app.css') }}" rel="stylesheet">
     <link href="https://file.myfontastic.com/cmbgRkayBnHXSnPo8TJuHj/icons.css" rel="stylesheet">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
     <!--FontAwesome-->
     <script src="https://use.fontawesome.com/8eb4ed0827.js"></script>
     {{--<!-- Latest compiled and minified JavaScript -->--}}
