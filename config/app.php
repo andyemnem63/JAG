38e0f8fdfe6483dc51ec0a9dc0ad24e2e6da03f2
Glen Turner <gturner0316@yahoo.com>
2017-08-15 19:38:33 -0400

    style

--- config/app.php
+++ config/app.php
@@ -163,6 +163,7 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
         Laravel\Socialite\SocialiteServiceProvider::class,
+        Barryvdh\Cors\ServiceProvider::class,
 
         /*
          * Package Service Providers...
