5794e76d6e6d1e62515016e5abde6f0eaa5f6a75
Glen Turner <gturner0316@yahoo.com>
2017-08-15 19:10:31 -0400

    style

--- resources/views/pages/days.blade.php
+++ resources/views/pages/days.blade.php
@@ -71,7 +71,7 @@
     <h3>Polls</h3>
     @foreach($allPolls as $polls)
         @if($polls->invite_id == $trip_id)
-            <a href="/pollResults/{{$trip_id}}/{{$polls->id}}" class="btn btn-default">{{$polls->poll_message}} {{$usersVoted}}/{{$totalUsers}}</a>
+            <a href="/pages.pollResults" class="btn btn-default">{{$polls->poll_message}} {{$usersVoted}}/{{$totalUsers}}</a>
         @endif
     @endforeach
 
