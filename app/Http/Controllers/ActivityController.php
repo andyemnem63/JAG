15cace90264e500969aa4208819ef5c1f01ef497
Glen Turner <gturner0316@yahoo.com>
2017-08-15 15:57:43 -0400

    migration for act table

--- app/Http/Controllers/ActivityController.php
+++ app/Http/Controllers/ActivityController.php
@@ -10,6 +10,7 @@ class ActivityController extends Controller
     public function post(Request $request)
     {
         $activityTable = new Act;
+        $activityTable ->user_id = $request->user->id;
         $activityTable->activity_name= $request->activity_name;
         $activityTable->url= $request->url;
         $activityTable->imgUrl= $request->imgUrl;
