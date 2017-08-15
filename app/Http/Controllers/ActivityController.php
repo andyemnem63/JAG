892c7f8c212a05112b38cc6a31e3372767d36589
Glen Turner <gturner0316@yahoo.com>
2017-08-15 16:21:01 -0400

    user id

--- app/Http/Controllers/ActivityController.php
+++ app/Http/Controllers/ActivityController.php
@@ -10,7 +10,7 @@ class ActivityController extends Controller
     public function post(Request $request)
     {
         $activityTable = new Act;
-        $activityTable ->user_id = $request->user->id;
+        $activityTable ->user_id = $request->user()->id;
         $activityTable->activity_name= $request->activity_name;
         $activityTable->url= $request->url;
         $activityTable->imgUrl= $request->imgUrl;
